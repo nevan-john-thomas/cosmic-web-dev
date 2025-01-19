ca3e9df8531bde2f162f1e95d83e632ae6236f5a
Maher Abdul Gafoor <maher.ag48@gmail.com>
2025-01-19 17:58:18 +0400

    more random

--- sketch.js
+++ sketch.js
@@ -101,19 +101,19 @@ function keyPressed() {
                 point(...coord);
             }
         }
-        // console.log("ENTER Pressed");
-        //   // Night-sky gradient
-        // let gradientArray = [[255, 62, 26], [250, 41, 56]]; // color values are provided as HSB
+         console.log("ENTER Pressed");
+           // Night-sky gradient
+         let gradientArray = [[255, 62, 26], [250, 41, 56]]; // color values are provided as HSB
 
-        // let startX = 0;
-        // let startY = 0;
-        // let endX = CANVAS_WIDTH;
-        // let endY = CANVAS_HEIGHT;
+         let startX = 0;
+         let startY = 0;
+         let endX = CANVAS_WIDTH;
+         let endY = CANVAS_HEIGHT;
 
-        // drawGradientBackground(gradientArray, startX, startY, endX, endY);
+         drawGradientBackground(gradientArray, startX, startY, endX, endY);
         
-        // // Adds a dark transparent rectangle to make the night-sky appear darker
-        // fill(0, 0, 0, 55);
-        // rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+         // Adds a dark transparent rectangle to make the night-sky appear darker
+         fill(0, 0, 0, 55);
+         rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
 }
